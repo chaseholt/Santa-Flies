1c3f3a63b01d212f56b5b76aa77ecd696e280b16
Chase Holt <chaseholt@orc3.com>
2021-12-15 14:59:42 -0500

    Added login functionality

--- client/index.js
+++ client/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { render } from "react-dom";
 import AppContainer from "./containers/AppContainer";
-// import socket from "socket.io-client";
-// const clientSocket = socket(window.location.origin);
-// clientSocket.on("connect", () => {
-//   console.log("connected to server");
-// });
-
+import socket from "socket.io-client";
+const clientSocket = socket(window.location.origin);
+clientSocket.on("connect", () => {
+  console.log("connected to server");
+});
 render(<AppContainer />, document.getElementById("app"));
---
+++ server/api/rooms.js
@@ -0,0 +1,52 @@
+const router = require("express").Router();
+const {
+  models: { User, Room },
+} = require("../db");
+module.exports = router;
+
+//get room with name
+router.get("/", async (req, res, next) => {
+  try {
+    const room = await Room.findOne({
+      where: {
+        roomName: req.body.roomName,
+      },
+    });
+    res.json(room);
+  } catch (err) {
+    next(err);
+  }
+});
+
+//get high schore of room
+router.get("/highscore", async (req, res, next) => {
+  try {
+    const room = await Room.findOne({
+      where: {
+        roomName: req.body.roomName,
+      },
+    });
+    res.json(room);
+  } catch (err) {
+    next(err);
+  }
+});
+
+//set high score of room
+router.post("/highscore", async (req, res, next) => {
+  try {
+    const room = await Room.findOne({
+      where: {
+        roomName: req.body.roomName,
+      },
+    });
+    room.update({
+      highScore: req.body.highScore,
+    });
+    res.send(room);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
